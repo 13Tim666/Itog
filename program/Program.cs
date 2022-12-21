350c2c9a25b172701533a2c21e3a308a4f62c39c
Teymur <agaev.tejmur@mail.ru>
2022-12-21 21:42:13 +0300

    recorded the program

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+string [] stringArray = {"12345", "-3", "hello", "MMM", "a1b2", "r6"};
+
+int LengthArray(string [] stringArray){
+    int count = 0;
+    for (int i = 0; i < stringArray.Length; i++){
+        if (stringArray[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string [] stringNewArray = new string[LengthArray(stringArray)];
+
+void FillArray(string [] stringNewArray, string [] stringArray){
+    int j = 0;
+    for (int i = 0; i < stringArray.Length; i++){
+        if (stringArray[i].Length <= 3) {
+            stringNewArray[j] = stringArray[i];
+            j++;
+        }
+    }
+}
+
+void Printarray(string [] array){
+        Console.Write("[ ");
+    for(int i = 0; i < array.Length; i++){
+            Console.Write($"{array[i]}, ");
+    }
+        Console.WriteLine("\b\b ]");
+}
+
+FillArray(stringNewArray, stringArray);
+Printarray(stringNewArray);
